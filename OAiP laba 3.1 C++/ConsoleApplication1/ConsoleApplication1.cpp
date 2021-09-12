8ea8c983c17c41e8d44da77b4e38d1389025f846
amplerented <dan.danya20@gmail.com>
2021-09-12 18:59:38 +0300

    init commit

---
+++ OAiP laba 3.1 C++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+
+{
+	double d , a , y , t , c , j ;
+	cout << " d = " ; cin>> d ;
+	cout << " a = "; cin >> a ;
+	cout << " c = " ; cin>> c ;
+	cout << " j = " ; cin >> j ;
+	t = (d * c + a * sqrt(c - 1)) ; 
+	y = 0.5 * t / d + exp(a);
+	cout << " t = " << t ;
+	cout << " y = " << y ;
+	return 0;
+}
